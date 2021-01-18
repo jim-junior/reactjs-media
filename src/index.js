666c732eab0d1a0012785533e1449d648f465560
Jim Junior <jimjunior854@gmail.com>
2021-01-19 01:52:27 +0300

    webpack config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 
-export const {default: ReactVideo} = require('./video/react-video')
+//export const {default: ReactVideo} = require('./video/react-video')
 
+export const { default: ReactVideo } = require("./video/react-video");
